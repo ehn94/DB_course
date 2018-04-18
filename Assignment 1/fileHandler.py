a497c4e0641abf081424aab6b831b64a8d80cb9e
Emilie <ehn1994@gmail.com>
2018-04-18 09:16:17 +0200

    added binary func

--- Assignment 1/fileHandler.py
+++ Assignment 1/fileHandler.py
@@ -6,10 +6,16 @@ param1 = sys.argv[1]
 param2 = sys.argv[2]
 
 def write_to_file(key=param1, val=param2):
-    db.write("{} {}".format(key, val))
-    pair = key, val
-    print pair
+    pair =  "%s:%s" % (key, val)
+    db.write(enc(pair) + '\n')
     db.close
+
+with open('db.txt', "rb") as binary_file:
+    data = binary_file.read()
+    print(data)
+
+def enc(string):
+    return "".join([format(ord(char), '#010b')[2:] for char in string])
 
 if __name__ == "__main__":
     write_to_file()
