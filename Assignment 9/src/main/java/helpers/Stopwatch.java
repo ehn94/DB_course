8694c9d9757087cf6dc106546c12f49478905080
Emilie <ehn1994@gmail.com>
2018-04-24 15:46:52 +0200

    stopwatch added

---
+++ Assignment 9/src/main/java/helpers/Stopwatch.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+/**
+ *
+ * @author ehn19
+ */
+public class Stopwatch {
+
+    private long start;
+
+    public Stopwatch() {
+        start = System.currentTimeMillis();
+    }
+    
+    public double elapseTime(){
+        long current = System.currentTimeMillis();
+        return (current - start) /1000.0;
+    }
+    public void reset(){
+        start = System.currentTimeMillis();
+    }
+}
